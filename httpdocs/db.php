4998c6568b4a46b41a3bb85342758e7c78f22bf3
Aditya Rohan <aditya.rohan@urbanladder.com>
2016-10-09 10:50:08 +0530

    db now pointing to aws server

--- httpdocs/db.php
+++ httpdocs/db.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', '182.50.133.80:3306');
-define('DB_USERNAME', 'vijay');
-define('DB_PASSWORD', 'Maatara1962');
+define('DB_SERVER', '172.31.9.31:3306');
+define('DB_USERNAME', 'dealyatradbuser');
+define('DB_PASSWORD', 'dealdb123');
 define('DB_DATABASE', 'idealeq_users');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
